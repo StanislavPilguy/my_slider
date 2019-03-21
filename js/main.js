2a1350b5b1d98d5c69b5a0eb31177e82c7fdefd8
Sanislav <stanislav.pilguy@gmail.com>
2019-03-21 20:56:59 +0200

    completion

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ let slideIndex = 0;
 const showSlides = () => {
 
 	const slides = document.getElementsByClassName('mySlides');
+	const dots = document.getElementsByClassName('dot');
 
 	for(let i = 0; i < slides.length; i++) {
 		slides[i].style.display = 'none';
@@ -14,7 +15,12 @@ const showSlides = () => {
 		slideIndex = 1;
 	}
 
+	for(let i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(' active', '');
+	}
+
 	slides[slideIndex - 1].style.display = 'block';
+	dots[slideIndex - 1].className+=' active';
 
 	setTimeout(showSlides, 3000);
 
